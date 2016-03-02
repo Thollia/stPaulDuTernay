f1474f57e50fa7af35f4fed62c2d7cbd148df8cc
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2016-03-02 08:34:05 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,1 @@
+##St Paul Du Terray
--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 $bdd = new PDO('mysql:host=localhost;dbname=bdstpaul;charset=utf8', 'root');
 
 include_once "model.php";
-
 $sejours = getSejour($bdd);
-
 include_once "view.php";
 ?>
