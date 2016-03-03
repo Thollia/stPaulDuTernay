7ad51afe7cbcab790c3acd761ccf5601b69b180c
pdubreuil <precillia.dubreuil@gmail.com>
2016-03-03 10:45:04 +0100

    Mise en place Silex

--- web/index.php
+++ web/index.php
@@ -1,7 +1,17 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=bdstpaul;charset=utf8', 'root');
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+require __DIR__.'/../app/routes.php';
+
+$app->run();
+
+
+/*$bdd = new PDO('mysql:host=localhost;dbname=bdstpaul;charset=utf8', 'root');
 
 include_once "model.php";
 $sejours = getSejour($bdd);
-include_once "view.php";
+include_once "view.php";*/
 ?>
