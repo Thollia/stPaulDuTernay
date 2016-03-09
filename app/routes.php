18774fa5f5c8e62f65b7de23ba748ece91d17eb4
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2016-03-09 08:33:58 +0100

    Ajout de la route index

--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,12 @@ $app->get('/', function() use ($app) {
 
 });
 
+$app->get('/index', function() use ($app) {
+    $sejours = $app['dao.sejour']->findAll();
+    return $app['twig']->render('index.html.twig', array('sejours' => $sejours));
+
+});
+
 $app->get('/contact', function() use ($app){
     return $app['twig']->render('contact.html.twig');
 });
