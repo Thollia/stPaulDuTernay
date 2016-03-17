b56108cdef644c8692e04f98a2ea7f8b49ea6d68
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2016-03-17 10:38:38 +0100

    Finalisation des tests

--- test/Domain/SimulationTest.php
+++ test/Domain/SimulationTest.php
@@ -74,11 +74,140 @@ class SimulationTest extends \PHPUnit_Framework_TestCase
     }
 
     public function testQuotientFamilialSup500(){
+        $this->build(1,1,550);
 
+        $resultatAttenduSejour = 0;
+
+        $this->simulation->calculReducQuotientFamilial();
+
+        $this->assertEquals($resultatAttenduSejour, $this->simulation->getReducQuotientFamilial());
     }
 
     public function testQuotientFamilialInf500(){
+        $this->build(1,1,450);
 
+        $resultatAttenduSejour = 50;
+
+        $this->simulation->calculReducQuotientFamilial();
+
+        $this->assertEquals($resultatAttenduSejour, $this->simulation->getReducQuotientFamilial());
+    }
+
+    public function testSousTotal(){
+        $this->build(1,1,450);
+        $this->simulation->setReducQuotientFamilial(25);
+        $this->simulation->setReducNombreEnfant(25);
+
+        $resultatAttendu = 450;
+
+        $this->simulation->calculSousTotal();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getSousTotal());
+    }
+
+    public function testReducDepartUnEnfant(){
+        $this->build(1,1,450);
+
+        $this->simulation->setSousTotal(450);
+
+        $resultatAttendu = 0;
+
+        $this->simulation->calculDepartPlsEnfants();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getReducDepartPlsEnfant());
+    }
+
+    public function testReducDepartPlusieursEnfant(){
+        $this->build(1,2,450);
+
+        $this->simulation->setSousTotal(450);
+
+        $resultatAttendu = 45;
+
+        $this->simulation->calculDepartPlsEnfants();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getReducDepartPlsEnfant());
+    }
+
+    public function testPrixApresReduc(){
+        $this->build(1,2,450);
+
+        $this->simulation->setSousTotal(450);
+        $this->simulation->setReducDepartPlsEnfant(45);
+
+        $resultatAttendu = 405;
+
+        $this->simulation->calculPrixApresReduction();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getPrixApresReduction());
+    }
+
+    public function testPlafonnementSup100(){
+        $this->build(1,2,450);
+
+        $this->simulation->setPrixApresReduction(200);
+
+        $resultatAttendu = 100;
+
+        $this->simulation->calculNetAPayer();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getNetAPayer());
+    }
+
+    public function testPlafonnementInf100(){
+        $this->build(1,2,450);
+
+        $this->simulation->setPrixApresReduction(90);
+
+        $resultatAttendu = 90;
+
+        $this->simulation->calculNetAPayer();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getNetAPayer());
+    }
+
+    public function testTotalDepartUnEnfant(){
+        $this->build(1,1,450);
+
+        $this->simulation->setNetAPayer(90);
+
+        $resultatAttendu = 90;
+
+        $this->simulation->calculTotalDepartMultiple();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getTotalDepartMultiple());
+    }
+
+    public function testTotalDepartDeuxEnfant(){
+        $this->build(1,2,450);
+
+        $this->simulation->setNetAPayer(90);
+
+        $resultatAttendu = 180;
+
+        $this->simulation->calculTotalDepartMultiple();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getTotalDepartMultiple());
+    }
+
+    public function testUltimeUnEnfant(){
+        $this->build(1,1,450);
+
+        $resultatAttendu = 100;
+
+        $this->simulation->calculTotal();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getTotalDepartMultiple());
+    }
+
+    public function testUltimeDixEnfant(){
+        $this->build(1,10,450);
+
+        $resultatAttendu = 1000;
+
+        $this->simulation->calculTotal();
+
+        $this->assertEquals($resultatAttendu, $this->simulation->getTotalDepartMultiple());
     }
 
 }
